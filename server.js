bf88f82dfe3a1e4bf13b317d9973282872a6beb3
Han-seohee <a67114585a@gmail.com>
2021-10-02 21:50:17 +0900

    Implement Customer ADD Module & Customer List Refresh Module

--- server.js
+++ server.js
@@ -24,11 +24,31 @@ connection.connect();
 // const connection = require('mysql');
 // 강제로 노드 모듈 고치기 : npm audit fix --force
 
+const multer = require('multer');
+const upload = multer({dest: './upload'})
+
 app.get('/api/customers', (req, res) => {
     connection.query(
         'SELECT * FROM CUSTOMER', //이부분
         (err, rows, fields) => {
-            console.log('test: '+ rows);
+            // console.log('test: '+ rows);
+            res.send(rows);
+        }
+    );
+});
+
+app.use('./image', express.static('./upload'));
+
+app.post('./api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params,
+        (err, row, fields) => {
             res.send(rows);
         }
     );
